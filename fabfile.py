0f289c8008bcee746ded1c6f9b009c1dd85a02e3
moregale <moregale@e34f5c4e-b901-438e-bb7f-4482d47b2943>
2012-06-29 10:00:30 +0000

    deployment using fabric and sarge
    git-svn-id: https://svn.eionet.europa.eu/repositories/GioLand/gioland/trunk@10 e34f5c4e-b901-438e-bb7f-4482d47b2943

---
+++ fabfile.py
@@ -0,0 +1,70 @@
+from StringIO import StringIO
+from fabric.api import *
+from fabric.contrib.files import exists
+from path import path
+
+
+env.update({
+    'hosts': ['gaur'],
+    'use_ssh_config': True,
+    'python_prefix': path('/var/local/gioland/workflow/Python-2.7.3'),
+    'sarge_home': path('/var/local/gioland/workflow/sarge'),
+    'gioland_venv': path('/var/local/gioland/workflow/sarge/var/gioland-venv'),
+})
+
+
+def sarge(cmd):
+    sarge_base = ("'%(sarge_home)s/venv/bin/sarge' "
+                  "'%(sarge_home)s' " % env)
+    return run(sarge_base + cmd)
+
+
+def version_paths(version_folder):
+    return {
+        'gioland_version_folder': version_folder,
+        'gioland_version_instance': version_folder/'instance',
+    }
+
+
+@task
+def ssh():
+    open_shell()
+
+
+@task
+def virtualenv():
+    if not exists(env['gioland_venv']):
+        run("'%(python_prefix)s/bin/virtualenv' '%(gioland_venv)s'" % env)
+
+    put("requirements.txt", str(env['gioland_venv']))
+    run("%(gioland_venv)s/bin/pip install "
+        "-r %(gioland_venv)s/requirements.txt"
+        % env)
+
+
+@task
+def install():
+    if not exists("%(gioland_version_folder)s/.git" % env):
+        run("git init '%(gioland_version_folder)s'" % env)
+
+    local("git push -f '%(host_string)s:%(gioland_version_folder)s' "
+          "HEAD:incoming" % env)
+    with cd(env['gioland_version_folder']):
+        run("git reset incoming --hard")
+
+    if not exists(env['gioland_version_instance']):
+        run("mkdir -p '%s'" % env['gioland_version_instance'])
+
+    put(StringIO("exec %(gioland_venv)s/bin/python "
+                 "manage.py runcherrypy -p 42023\n" % env),
+        str(env['gioland_version_folder']/'start.sh'))
+
+    put(StringIO("{}"), str(env['gioland_version_folder']/'sargeapp.yaml'))
+
+
+@task
+def deploy():
+    version_folder = path(sarge("new_version gioland"))
+    with settings(**version_paths(version_folder)):
+        execute('install')
+    sarge("activate_version gioland '%s'" % version_folder)
--- manage.py
+++ manage.py
@@ -26,5 +26,42 @@ def create_app(config={}):
 manager = flaskext.script.Manager(create_app)
 
 
+class RunCherryPyCommand(flaskext.script.Command):
+
+    option_list = [
+        flaskext.script.Option('--port', '-p', dest='port', type=int),
+    ]
+
+    def handle(self, app, port):
+        import sys
+        import os
+        import logging
+        from path import path
+
+        with open(os.environ['SARGEAPP_CFG'], 'rb') as f:
+            sargeapp_cfg = flask.json.load(f)
+        warehouse_path = path(sargeapp_cfg['services'][0]['path'])
+
+        import warehouse
+        app.config['WAREHOUSE_PATH'] = warehouse_path
+        app.extensions['warehouse_connector'] = \
+            warehouse.WarehouseConnector(app.config['WAREHOUSE_PATH'])
+
+        handler = logging.StreamHandler()
+        handler.level = logging.INFO
+        logging.getLogger().addHandler(handler)
+
+        import cherrypy.wsgiserver
+        listen = ('127.0.0.1', port)
+        server = cherrypy.wsgiserver.CherryPyWSGIServer(listen, app.wsgi_app)
+        try:
+            server.start()
+        except KeyboardInterrupt:
+            server.stop()
+
+
+manager.add_command('runcherrypy', RunCherryPyCommand())
+
+
 if __name__ == '__main__':
     manager.run()
