51c29061fc1f746eed440de3abbe9ee43e66c330
moregale <moregale@e34f5c4e-b901-438e-bb7f-4482d47b2943>
2012-07-02 12:20:51 +0000

    wrap the app in reverse proxy fixup middleware
    git-svn-id: https://svn.eionet.europa.eu/repositories/GioLand/gioland/trunk@27 e34f5c4e-b901-438e-bb7f-4482d47b2943

--- manage.py
+++ manage.py
@@ -23,6 +23,31 @@ def create_app(config={}):
     return app
 
 
+class ReverseProxied(object):
+    # working behind a reverse proxy (http://flask.pocoo.org/snippets/35/)
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+
+        host = environ.get('HTTP_X_FORWARDED_HOST', '')
+        if host:
+            environ['HTTP_HOST'] = host
+
+        return self.app(environ, start_response)
+
+
 manager = flaskext.script.Manager(create_app)
 
 
@@ -53,7 +78,8 @@ class RunCherryPyCommand(flaskext.script.Command):
 
         import cherrypy.wsgiserver
         listen = ('127.0.0.1', port)
-        server = cherrypy.wsgiserver.CherryPyWSGIServer(listen, app.wsgi_app)
+        wsgi_app = ReverseProxied(app.wsgi_app)
+        server = cherrypy.wsgiserver.CherryPyWSGIServer(listen, wsgi_app)
         try:
             server.start()
         except KeyboardInterrupt:
