a26f879e695de083fd0b8c2a22d98ec8ed6baee1
moregale <moregale@e34f5c4e-b901-438e-bb7f-4482d47b2943>
2012-07-09 15:35:40 +0000

    log warehouse actions to a file
    git-svn-id: https://svn.eionet.europa.eu/repositories/GioLand/gioland/trunk@40 e34f5c4e-b901-438e-bb7f-4482d47b2943

--- warehouse.py
+++ warehouse.py
@@ -1,12 +1,19 @@
 import tempfile
 from contextlib import contextmanager
 from datetime import datetime
+import logging, logging.handlers
 from persistent.list import PersistentList
 from persistent.mapping import PersistentMapping
 from BTrees.OOBTree import OOBTree
 from persistent import Persistent
 import transaction
 from path import path
+
+
+log = logging.getLogger(__name__)
+
+LOGGING_FORMAT = '[%(asctime)s] %(levelname)s %(message)s'
+log_number = 1
 
 
 def _ensure_unicode(thing):
@@ -40,6 +47,8 @@ class Parcel(Persistent):
         self.metadata = PersistentMapping()
 
     def save_metadata(self, new_metadata):
+        self._warehouse.logger.info("Metadata update for %r: %r",
+                                    self.name, new_metadata)
         for key, value in new_metadata.iteritems():
             self.metadata[_ensure_unicode(key)] = _ensure_unicode(value)
 
@@ -50,13 +59,21 @@ class Parcel(Persistent):
         return self.get_path().listdir()
 
     def finalize(self):
+        self._warehouse.logger.info("Finalizing %r", self.name)
         self.save_metadata({'upload_time': datetime.utcnow().isoformat()})
 
 
 class Warehouse(Persistent):
 
-    # `fs_path` is injected at runtime, not stored in the database
-    __slots__ = ('fs_path', '__dict__')
+    _volatile_attributes = {}
+
+    @property
+    def fs_path(self):
+        return self._volatile_attributes[id(self)]['fs_path']
+
+    @property
+    def logger(self):
+        return self._volatile_attributes[id(self)]['logger']
 
     def __init__(self):
         self._parcels = OOBTree()
@@ -69,9 +86,11 @@ class Warehouse(Persistent):
         parcel_path = path(tempfile.mkdtemp(prefix='', dir=self.parcels_path))
         parcel = Parcel(self, parcel_path.name)
         self._parcels[parcel.name] = parcel
+        self.logger.info("New parcel %r", parcel.name)
         return parcel
 
     def delete_parcel(self, name):
+        self.logger.info("Deleting parcel %r", name)
         parcel = self._parcels.pop(name)
         parcel.get_path().rmtree()
 
@@ -98,11 +117,18 @@ class WarehouseConnector(object):
         return self._db
 
     def get_warehouse(self):
+        global log_number
+
         conn = self._get_db().open()
         transaction.begin()
+        handler = logging.handlers.WatchedFileHandler(self._fs_path/'activity.log')
+        handler.setLevel(logging.INFO)
+        handler.setFormatter(logging.Formatter(LOGGING_FORMAT))
 
         def cleanup():
             transaction.abort()
+            warehouse.logger.removeHandler(handler)
+            del warehouse._volatile_attributes[id(warehouse)]
             conn.close()
 
         zodb_root = conn.root()
@@ -110,7 +136,13 @@ class WarehouseConnector(object):
             zodb_root['warehouse'] = Warehouse()
 
         warehouse = zodb_root['warehouse']
-        warehouse.fs_path = _ensure_dir(self._fs_path)
+        warehouse._volatile_attributes[id(warehouse)] = {
+            'fs_path': _ensure_dir(self._fs_path),
+            'logger': log.getChild('con%d' % log_number),
+        }
+        warehouse.logger.setLevel(logging.INFO)
+        log_number += 1
+        warehouse.logger.addHandler(handler)
         _ensure_dir(warehouse.parcels_path)
 
         return warehouse, cleanup
