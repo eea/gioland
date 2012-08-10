0fdc54a98335f5605be5bb1fd5e7603e802fa356
Dragos Catarahia <dragos.catarahia@gmail.com>
2012-08-10 12:18:57 +0300

    Tests for upload parcel files

--- tests/upload_test.py
+++ tests/upload_test.py
@@ -1,6 +1,6 @@
+import json
 from StringIO import StringIO
-import flask
-from common import AppTestCase, authorization_patch
+from common import AppTestCase, authorization_patch, select
 
 
 class UploadTest(AppTestCase):
@@ -8,87 +8,144 @@ class UploadTest(AppTestCase):
     CREATE_WAREHOUSE = True
 
     def setUp(self):
-        self.parcels_path = self.wh_path/'parcels'
+        self.client = self.app.test_client()
+        self.parcels_path = self.wh_path / 'parcels'
         self.addCleanup(authorization_patch().stop)
 
-    def test_login(self):
-        client = self.app.test_client()
-        client.post('/test_login', data={'username': 'tester'})
+    def try_upload(self, name, filename='data.gml'):
+        data = {
+            'resumableFilename': filename,
+            'resumableIdentifier': 'data_gml',
+            'resumableTotalSize': '11',
+        }
 
-        client.preserve_context = True
-        client.get('/')
-        self.assertEqual(flask.g.username, 'tester')
+        chunk_1_data = dict(data)
+        chunk_1_data['resumableChunkSize'] = '3'
+        chunk_1_data['resumableChunkNumber'] = '1'
+        chunk_1_data['file'] = (StringIO('teh'), filename)
 
-    def test_begin_parcel_creates_folder(self):
-        client = self.app.test_client()
-        resp = client.post('/parcel/new', data=self.PARCEL_METADATA)
-        self.assertIsNotNone(resp.location)
+        chunk_2_data = dict(data)
+        chunk_2_data['resumableChunkSize'] = '8'
+        chunk_2_data['resumableChunkNumber'] = '2'
+        chunk_2_data['file'] = (StringIO('map data'), filename)
+
+        # upload chunks
+        resp_1 = self.client.post('/parcel/%s/file' % name, data=chunk_1_data)
+        resp_2 = self.client.post('/parcel/%s/file' % name, data=chunk_2_data)
+
+        if (resp_1.status_code, resp_2.status_code) == (200, 200):
+            # finalize upload
+            resp_3 = self.client.post('/parcel/%s/finalize_upload' % name,
+                                      data=data)
+            if resp_3.status_code != 200:
+                self.fail('finalize upload failed')
+            return True
+        elif (resp_1.status_code, resp_2.status_code) == (403, 403):
+            return False
+        elif (resp_1.status_code, resp_2.status_code) == (415, 415):
+            return False
+        else:
+            self.fail('unexpected http status code')
+
+    def try_upload_chunk(self, name, filename='data.gml'):
+        data = {
+            'resumableFilename': filename,
+            'resumableIdentifier': 'data_gml',
+            'resumableTotalSize': '11',
+        }
+
+        chunk_1_data = dict(data)
+        chunk_1_data['resumableChunkSize'] = '3'
+        chunk_1_data['resumableChunkNumber'] = '1'
+        chunk_1_data['file'] = (StringIO('teh'), filename)
+
+        resp = self.client.post('/parcel/%s/file' % name,  data=chunk_1_data)
+        if resp.status_code == 200:
+            return True
+        else:
+            return False
+
+    def create_parcel_at_stage(self, stage='sch'):
+        with self.app.test_request_context():
+            parcel = self.wh.new_parcel()
+            parcel.save_metadata({'stage': stage})
+        return parcel
+
+    def test_upload_file(self):
+        parcel = self.create_parcel_at_stage()
+        self.assertTrue(self.try_upload(parcel.name))
+
+    def test_reupload_file_not_allowed(self):
+        parcel = self.create_parcel_at_stage()
+        self.try_upload(parcel.name)
+        self.assertFalse(self.try_upload(parcel.name))
+
+    def test_upload_file_on_final_stage_forbidden(self):
+        parcel = self.create_parcel_at_stage(stage='fva')
+        self.assertFalse(self.try_upload(parcel.name))
+
+    def test_delete_file(self):
+        parcel = self.create_parcel_at_stage()
+        resp = self.client.post('/parcel/%s/file/%s/delete' %
+                                (parcel.name, 'data.gml'))
+        self.assertEqual(302, resp.status_code)
+
+    def test_finalized_parcel_forbids_deletion(self):
+        with self.app.test_request_context():
+            parcel = self.wh.new_parcel()
+            parcel.save_metadata({'stage': 'fva'})
+            parcel.finalize()
+        resp = self.client.post('/parcel/%s/file/%s/delete' % (parcel.name,
+                                                               'data.gml'))
+        self.assertEqual(403, resp.status_code)
+
+    def test_files_view(self):
+        parcel = self.create_parcel_at_stage()
+        self.try_upload(parcel.name)
+        resp = self.client.get('/parcel/%s/files' % parcel.name)
+        self.assertEqual(1, len(select(resp.data, 'ul li')))
+
+    def test_check_chunk_view(self):
+        parcel = self.create_parcel_at_stage()
+        self.try_upload_chunk(parcel.name)
+        url = '/parcel/%s/file?resumableFilename=data.gml'\
+              '&resumableIdentifier=data_gml&resumableTotalSize=11'\
+              '&resumableChunkNumber=1&resumableChunkSize=3' % parcel.name
+        resp = self.client.get(url)
+        self.assertEqual(200, resp.status_code)
+
+    def test_upload_chunk_create_temp_folder(self):
+        parcel = self.create_parcel_at_stage()
+        self.try_upload_chunk(parcel.name)
+
+        with self.app.test_request_context():
+            parcel = self.wh.get_parcel(parcel.name)
+            dirs = parcel.get_path().dirs()
+            self.assertEqual(1, len(dirs))
+
+    def test_finalize_parcel_remove_temp_folders(self):
+        resp = self.client.post('/parcel/new', data=self.PARCEL_METADATA)
         parcel_name = resp.location.rsplit('/', 1)[-1]
-        self.assertTrue((self.parcels_path/parcel_name).isdir())
 
-    def test_begin_parcel_saves_user_selected_metadata(self):
-        client = self.app.test_client()
-        resp = client.post('/parcel/new', data=dict(self.PARCEL_METADATA, bogus='not here'))
-        parcel_name = resp.location.rsplit('/', 1)[-1]
+        self.try_upload_chunk(parcel_name)
+        resp = self.client.post('/parcel/%s/finalize' % parcel_name)
+
         with self.app.test_request_context():
             parcel = self.wh.get_parcel(parcel_name)
-            self.assertDictContainsSubset(self.PARCEL_METADATA, parcel.metadata)
-            self.assertNotIn('bogus', parcel.metadata)
+            dirs = parcel.get_path().dirs()
+            self.assertEqual(0, len(dirs))
 
-    def test_begin_parcel_saves_default_metadata(self):
-        client = self.app.test_client()
-        client.post('/test_login', data={'username': 'somebody'})
-        resp = client.post('/parcel/new', data=self.PARCEL_METADATA)
-        parcel_name = resp.location.rsplit('/', 1)[-1]
-        with self.app.test_request_context():
-            parcel = self.wh.get_parcel(parcel_name)
-            self.assertEqual(parcel.metadata['stage'], 'int')
+    def finalize_upload_fails_if_chunks_are_not_uploaded(self):
+        data = {
+            'resumableFilename': 'data.gml',
+            'resumableIdentifier': 'data_gml',
+            'resumableTotalSize': '11',
+        }
 
-    def test_show_existing_files_in_parcel(self):
-        client = self.app.test_client()
-        resp = client.post('/parcel/new', data=self.PARCEL_METADATA)
-        parcel_name = resp.location.rsplit('/', 1)[-1]
-        parcel_path = self.parcels_path/parcel_name
-        (parcel_path/'some.txt').write_text('hello world')
-
-        resp2 = client.get('/parcel/' + parcel_name)
-        self.assertIn('some.txt', resp2.data)
-
-    def test_http_post_saves_file_in_parcel(self):
-        client = self.app.test_client()
-        resp = client.post('/parcel/new', data=self.PARCEL_METADATA)
-        parcel_name = resp.location.rsplit('/', 1)[-1]
-        parcel_path = self.parcels_path/parcel_name
-
-        client.post('/parcel/' + parcel_name + '/file', data={
-            'file': (StringIO("teh file contents"), 'data.gml'),
-        })
-        self.assertEqual(parcel_path.listdir(), [parcel_path/'data.gml'])
-
-    def test_finalize_changes_parceling_flag(self):
-        client = self.app.test_client()
-        resp = client.post('/parcel/new', data=self.PARCEL_METADATA)
-        parcel_name = resp.location.rsplit('/', 1)[-1]
-
-        with self.app.test_request_context():
-            parcel = self.wh.get_parcel(parcel_name)
-            self.assertTrue(parcel.uploading)
-
-        resp2 = client.post('/parcel/%s/finalize' % parcel_name)
-        parcel_name = resp2.location.rsplit('/', 1)[-1]
-
-        with self.app.test_request_context():
-            parcel = self.wh.get_parcel(parcel_name)
-            self.assertFalse(parcel.uploading)
-
-    def test_uploading_in_finalized_parcel_is_not_allowed(self):
-        client = self.app.test_client()
-        resp = client.post('/parcel/new', data=self.PARCEL_METADATA)
-        parcel_name = resp.location.rsplit('/', 1)[-1]
-        parcel_path = self.parcels_path/parcel_name
-        client.post('/parcel/%s/finalize' % parcel_name)
-
-        resp2 = client.post('/parcel/' + parcel_name + '/file', data={
-            'file': (StringIO("teh file contents"), 'data.gml')})
-        self.assertEqual(resp2.status_code, 403)
-        self.assertEqual(parcel_path.listdir(), [])
+        parcel = self.create_parcel_at_stage()
+        self.try_upload_chunk(parcel.name)
+        resp = self.client.post('/parcel/%s/finalize_upload' % parcel.name,
+                                data=data)
+        self.assertEqual(200, resp.status_code)
+        response = json.loads(resp.data)
+        self.assertEqual('error', response['status'])
