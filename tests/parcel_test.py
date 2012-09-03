a5b76ab6a9558d5b65ecfe920ce05f3330dda525
moregale <moregale@e34f5c4e-b901-438e-bb7f-4482d47b2943>
2012-09-03 14:05:48 +0000

    fix unit test
    git-svn-id: https://svn.eionet.europa.eu/repositories/GioLand/gioland/trunk@137 e34f5c4e-b901-438e-bb7f-4482d47b2943

--- tests/parcel_test.py
+++ tests/parcel_test.py
@@ -296,7 +296,7 @@ class ParcelTest(AppTestCase):
         themes = select(resp.data, '.scope-row')
         self.assertEqual(2, len(themes))
 
-        themes_text = [t.text.strip() for t in themes]
+        themes_text = ''.join(t.text.strip() for t in themes)
         self.assertIn('Grassland Cover', themes_text)
         self.assertIn('Tree Type', themes_text)
 
